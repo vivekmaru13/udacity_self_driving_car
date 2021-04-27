72f5b05736e4e5be70f0c4bcf2f057dda925161c
VivekGhub <vivek_maru00128@yahoo.com>
2021-04-28 02:16:17 +0530

    P8 - PID Controller Project

---
+++ P8-PID_Control/src/PID.cpp
@@ -0,0 +1,42 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
+}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+  i_error += cte;
+  d_error = cte - p_error;
+  p_error = cte;
+
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  double control_sig = -(Kp * p_error + Ki * i_error + Kd * d_error);
+  
+  return control_sig;  // TODO: Add your total error calc here!
+}
